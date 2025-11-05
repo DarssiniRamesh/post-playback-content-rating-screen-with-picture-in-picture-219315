9e62503ef32dede80b7dc75b63001351ab3f2afb
Kavia AI <production@kavia.ai>
2025-11-05 18:49:32 +0000

    Checkpoint 1 - cgc440bec2

---
+++ post_playback_rating_frontend/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "post_playback_rating_frontend"
+include(":app")
